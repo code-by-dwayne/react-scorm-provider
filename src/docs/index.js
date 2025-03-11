a53df1e8cb9e14abf2fc75d7003f049c17f04bd3
Erik-EFL <erik.lima.f@outlook.com>
2025-03-11 02:02:30 -0300

    chore: :fire:

--- src/docs/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./components/App";
-import './styles/normalize.css';
-import './styles/skeleton.css';
-import './styles/styles.css';
-
-render(<App />, document.getElementById("app"));
--- src/lib/index.js
+++
@@ -1,5 +0,0 @@
-import withScorm from './withScorm';
-import ScormProvider from './ScormProvider';
-
-export { withScorm };
-export default ScormProvider;
